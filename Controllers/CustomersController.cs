b0381c641763796b94efbb0af4b217724344f7c5
Chalk <74837083+barmybrit@users.noreply.github.com>
2021-10-12 22:02:49 +0200

    Exercise 2 NO styling

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult CustomersListed()
+        {
+            List<Customer> customerList = new List<Customer>
+            {
+                new Customer { Name = "Richard"},
+                new Customer { Name = "Heimer"},
+                new Customer { Name = "Ghane"}
+            };
+
+            var viewModel = new ListsViewModel()
+            {
+                Customers = customerList
+            };
+
+
+            return View(viewModel);
+        }
+    }
+}
+
+
+
+
+
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult MoviesListed()
+        {
+            List<Movie> movieList = new List<Movie>
+            {
+                new Movie { Name = "Lord of the Rings"},
+                new Movie { Name = "The Two Towers"},
+                new Movie { Name = "The Return of the King"}
+            };
+
+            var viewModel = new ListsViewModel()
+            {
+                Movies = movieList
+            };
+
+           
+            return View(viewModel);
+        }
+
+    }
+}
